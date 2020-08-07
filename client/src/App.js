e8de6d62e84054ef0145529f023fc23e9cf878b5
Brad Williams <jb.wms89@gmail.com>
2020-08-07 11:08:08 -0400

    added navbar links with react router dom and filler h1s for each page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Nav from "../src/components/Nav/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../src/pages/Home/Home";
+import SignUP from "../src/pages/SignUp/SignUp";
+import Profile from "../src/pages/Profile/Profile";
+import Excursion from "../src/pages/Excursion/Excursion";
+import Inventory from "../src/pages/Inventory/Inventory";
 
 function App() {
   return (
     <div>
+      <Router>
       <Nav />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/SignUp" component={SignUP} />
+      <Route exact path="/Profile" component={Profile} />
+      <Route exact path="/Excursion" component={Excursion} />
+      <Route exact path="/Inventory" component={Inventory} />
+      </Router>
     </div>
   );
 }
