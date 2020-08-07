99795052778da771c13667bcee91821f2f36361c
Brad Williams <jb.wms89@gmail.com>
2020-08-07 10:29:03 -0400

    added style library

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    trim: true,
+    required: "Email is required.",
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is required.",
+  },
+  items: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Item",
+    },
+  ],
+  excursions: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Excursion",
+    },
+  ],
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
