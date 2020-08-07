e6b4ac1aa53ea98b89039b0bac477dc06d0a17c1
Brad Williams <jb.wms89@gmail.com>
2020-08-07 12:57:06 -0400

    added menudropdown and cleaned up navbar

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+import MenuDropdown from "../MenuDropdown/MenuDropdpown";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,34 +24,10 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h2" className={classes.title}>
             TrackPacker
           </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <Link to="/Home">
-              <Button color="inherit">Login</Button>
-            </Link>
-          </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <Link to="/SignUp">
-              <Button color="inherit">Sign-Up</Button>
-            </Link>
-          </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <Link to="/Profile">
-              <Button color="inherit">Profile</Button>
-            </Link>
-          </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <Link to="/Excursion">
-              <Button color="inherit">Excursion</Button>
-            </Link>
-          </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <Link to="/Inventory">
-              <Button color="inherit">Inventory</Button>
-            </Link>
-          </Typography>
+          <MenuDropdown />
         </Toolbar>
       </AppBar>
     </div>
