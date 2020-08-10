0fa5b55b06c5e881dbab1b3525d3308f91f87095
Brad Williams <jb.wms89@gmail.com>
2020-08-10 14:11:22 -0400

    merging

--- client/src/components/ToggleWishlist/ToggleWishlist.jsx
+++ client/src/components/ToggleWishlist/ToggleWishlist.jsx
@@ -4,8 +4,6 @@ import { green } from '@material-ui/core/colors';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import Favorite from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
--- client/src/pages/Inventory/Inventory.jsx
+++ client/src/pages/Inventory/Inventory.jsx
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import Avatar from "@material-ui/core/Avatar";
