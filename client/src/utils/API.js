30e9f351fcc2c2d15a752284d27ff9e2f06a5382
CheaC <cheacliatt@gmail.com>
2020-08-10 14:14:35 -0400

    Have things from excursion db showing up on excursion route

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,7 +10,7 @@ export default {
   },
 
   addItem: function() {
-    return axios.post("/api/items", { name, status });
+    return axios.post("/api/items");
   },
 
   getExcursions: function() {
@@ -22,7 +22,7 @@ export default {
   },
 
   addExcursion: function() {
-    return axios.post("/api/excursions", { name });
+    return axios.post("/api/excursions");
   },
 
 
