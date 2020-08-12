e21114ee4436c2e59a76e94d84faf06bb6cb0b02
Brad Williams <jb.wms89@gmail.com>
2020-08-12 12:12:54 -0400

    saving before merge

--- client/src/components/InventoryList/InventoryList.jsx
+++ client/src/components/InventoryList/InventoryList.jsx
@@ -4,7 +4,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-
 import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
