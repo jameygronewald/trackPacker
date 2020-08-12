35c5c5107f20c7625d1fe150193cc50cdbaab436
Justin <ponthierjustin@gmail.com>
2020-08-12 13:06:29 -0400

    added styling to front page for log in

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,20 +1,56 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
 
 const Home = () => {
-    return (
-        <div>
-            <form className="login">
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email-input" />
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password-input" />
-                <button type="submit">Log In</button>
-                <br></br>
-                <Link to="/SignUp">Sign Up!</Link>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <Container maxWidth="sm">
+        <Box m="2rem" p={2} mx="auto">
+          <form className="login">
+            <Box
+              alignItems="center"
+              justifyContent="center"
+              display="flex"
+              p={2}
+              mx="auto"
+            >
+              <TextField id="standard-basic" label="Email" />
+            </Box>
+            <Box
+              alignItems="center"
+              justifyContent="center"
+              display="flex"
+              p={2}
+              mx="auto"
+            >
+              <TextField id="standard-basic" label="Password" />
+            </Box>
+            <Box
+              alignItems="center"
+              justifyContent="center"
+              display="flex"
+              p={2}
+              mx="auto"
+            >
+              <Link type="submit">Log In</Link>
+            </Box>
+            <Box
+              alignItems="center"
+              justifyContent="center"
+              display="flex"
+              p={2}
+              mx="auto"
+            >
+              <Link to="/SignUp">Sign Up!</Link>
+            </Box>
+          </form>
+        </Box>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
