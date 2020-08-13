9e0b362542547f0cca2aad202a84ed4ec116917c
Justin <ponthierjustin@gmail.com>
2020-08-13 14:53:00 -0400

    adding accordion for inventory

--- client/src/pages/ExcursionInventory/ExcursionInventory.jsx
+++ client/src/pages/ExcursionInventory/ExcursionInventory.jsx
@@ -3,8 +3,14 @@ import Grid from "@material-ui/core/Grid";
 import API from "../../utils/API";
 import { useParams } from "react-router-dom";
 import Favorite from "@material-ui/icons/Favorite";
+import Box from "@material-ui/core/Box";
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const ExcursionInventory = props => {
+const ExcursionInventory = (props) => {
   const { id } = useParams();
 
   const [excursion, setExcursion] = useState({});
@@ -12,71 +18,118 @@ const ExcursionInventory = props => {
 
   useEffect(() => {
     API.getExcursion(id)
-      .then(response => {
+      .then((response) => {
         setExcursion(response.data.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
     API.getItems()
-      .then(response => {
+      .then((response) => {
         setInventory(response.data.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, [excursion]);
 
-  const addToExcursion = id => {
+  const addToExcursion = (id) => {
     excursion.items.push(id);
     let itemObj = { items: excursion.items };
     API.updateExcursionInventory(excursion._id, itemObj)
-      .then(response => {
+      .then((response) => {
         setExcursion(response.data.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   };
 
   return (
     <div>
-      <Grid container spacing={3}>
+      <Grid container spacing={2}>
         <Grid item xs={12}>
-          <h1>{excursion.name} Excursion</h1>
+          <Box
+            alignItems="center"
+            justifyContent="center"
+            display="flex"
+            p={2}
+            mx="auto"
+          >
+            <h1>{excursion.name}</h1>
+          </Box>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <h2>Inventory</h2>
-          <ul>
-            {inventory.map(item => (
-              <>
-                <li>{item.name}{item.status === "Wishlist" && <span><Favorite></Favorite></span>}</li>
-                <button
-                  onClick={() => {
-                    addToExcursion(item._id);
-                  }}
-                >
-                  Add to Excursion
-                </button>
-              </>
-            ))}
-          </ul>
+        <h2>Inventory</h2>
+          <Box
+          /*   alignItems="center"
+            justifyContent="center" */
+            display="flex"
+            p={2}
+            mx="auto"
+            flexDirection="row"
+            flexWrap="wrap"
+            p={1}
+            m={1}
+          >
+                  <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Brad's Inventory</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        <ul>
+              {inventory.map((item) => (
+                <>
+                  <li>
+                    {item.name}
+                    {item.status === "Wishlist" && (
+                      <span>
+                        <Favorite></Favorite>
+                      </span>
+                    )}
+                  </li>
+                  <button
+                    onClick={() => {
+                      addToExcursion(item._id);
+                    }}
+                  >
+                    Add to Excursion
+                  </button>
+                </>
+              ))}
+            </ul>
+        </AccordionDetails>
+      </Accordion>
+          </Box>
         </Grid>
         <Grid item xs={12} sm={6}>
           <h2>Inventory for {excursion.name}</h2>
           <ul>
             {excursion.items &&
               excursion.items
-                .filter(item => item.status === "Inventory")
-                .map(item => <li>{item.name}</li>)}
+                .filter((item) => item.status === "Inventory")
+                .map((item) => <li>{item.name}</li>)}
           </ul>
           <br></br>
           <h2>Wishlist for {excursion.name}</h2>
           <ul>
-          {excursion.items &&
+            {excursion.items &&
               excursion.items
-                .filter(item => item.status === "Wishlist")
-          .map(item => <li>{item.name}{item.status === "Wishlist" && <span><Favorite></Favorite></span>}</li>)}
+                .filter((item) => item.status === "Wishlist")
+                .map((item) => (
+                  <li>
+                    {item.name}
+                    {item.status === "Wishlist" && (
+                      <span>
+                        <Favorite></Favorite>
+                      </span>
+                    )}
+                  </li>
+                ))}
           </ul>
         </Grid>
       </Grid>
