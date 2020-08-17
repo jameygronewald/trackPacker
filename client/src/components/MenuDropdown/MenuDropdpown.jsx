6dddc20044f171b1b153b33689d20f32bee7a918
Brad Williams <jb.wms89@gmail.com>
2020-08-17 00:24:18 -0400

    saving b4 push

--- client/src/components/MenuDropdown/MenuDropdpown.jsx
+++ client/src/components/MenuDropdown/MenuDropdpown.jsx
@@ -77,7 +77,7 @@ export default function MenuListComposition() {
           variant="contained"
           className={classes.button}
         >
-          View my Profile
+          Logout
         </Button>
         <Popper
           open={open}
@@ -101,7 +101,7 @@ export default function MenuListComposition() {
                     id="menu-list-grow"
                     onKeyDown={handleListKeyDown}
                   >
-                    <Link to="/Profile">
+                    {/* <Link to="/Profile">
                       <MenuItem onClick={handleClose}>Profile</MenuItem>
                     </Link>
                     <Link to="/Inventory">
@@ -109,7 +109,7 @@ export default function MenuListComposition() {
                     </Link>
                     <Link to="/Excursions">
                       <MenuItem onClick={handleClose}>My Excursions</MenuItem>
-                    </Link>
+                    </Link> */}
                     <Link to="/">
                       <MenuItem onClick={handleLogout}>Logout</MenuItem>
                     </Link>
