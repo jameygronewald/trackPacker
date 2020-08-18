3e604e6ed5ca849f5c567a0f30f68d13e7537123
Jamey Gronewald <jrgronewald@gmail.com>
2020-08-17 19:55:31 -0700

    added bcrypt dependency and added functionality to hash password and save hashed password to db upon signup; then log in hashes password that's entered by user and compares to hashed db password to get token and user data back to front end

--- client/src/App.js
+++ client/src/App.js
@@ -24,10 +24,8 @@ function App() {
   };
 
   const getUserData = config => {
-    console.log(config);
     API.getUserInfo(config)
       .then(response => {
-        console.log(response);
         const userResponse = response.data.body.userObject;
         userResponse.isAuthenticated = true;
         setUserData(userResponse);
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 const { generateToken } = require("../client/src/utils/tokenHelper");
+const { genSalt, hash, compare } = require("bcrypt");
+const saltRounds = 10;
 
 // Login
 router.post("/api/login", (req, res) => {
@@ -10,22 +12,24 @@ router.post("/api/login", (req, res) => {
     .populate("items")
     .populate("excursions")
     .then(userData => {
-      if (userData.password === req.body.password) {
-        const token = generateToken(userData._id);
-        const userObject = {
-          firstName: userData.firstName,
-          lastName: userData.lastName,
-          items: userData.items,
-          excursions: userData.excursions,
-        };
-        res.status(201).json({
-          error: false,
-          body: { userObject, token },
-          message: "Successfully logged in.",
-        });
-      } else {
-        throw err;
-      }
+      compare(req.body.password, userData.password).then(result => {
+        if (result) {
+          const token = generateToken(userData._id);
+          const userObject = {
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            items: userData.items,
+            excursions: userData.excursions,
+          };
+          res.status(201).json({
+            error: false,
+            body: { userObject, token },
+            message: "Successfully logged in.",
+          });
+        } else {
+          throw err;
+        }
+      });
     })
     .catch(err => {
       res.status(401).json({
@@ -38,28 +42,38 @@ router.post("/api/login", (req, res) => {
 
 //Sign up
 router.post("/api/signup", (req, res) => {
-  db.User.create(req.body)
-    .then(userData => {
-      const token = generateToken(userData._id);
-      const userObject = {
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        items: userData.items,
-        excursions: userData.excursions,
-      };
-      res.status(201).json({
-        error: false,
-        body: { userObject, token },
-        message: "Successfully created account.",
+  const newUser = req.body;
+  genSalt(saltRounds, (err, salt) => {
+    hash(newUser.password, salt)
+      .then(hash => {
+        newUser.password = hash;
+        db.User.create(newUser)
+          .then(userData => {
+            const token = generateToken(userData._id);
+            const userObject = {
+              firstName: userData.firstName,
+              lastName: userData.lastName,
+              items: userData.items,
+              excursions: userData.excursions,
+            };
+            res.status(201).json({
+              error: false,
+              body: { userObject, token },
+              message: "Successfully created account.",
+            });
+          })
+          .catch(err => {
+            res.status(400).json({
+              error: true,
+              body: null,
+              message: "Unable to create account.",
+            });
+          });
+      })
+      .catch(err => {
+        res.json("Error saving password to database.");
       });
-    })
-    .catch(err => {
-      res.status(400).json({
-        error: true,
-        body: null,
-        message: "Unable to create account.",
-      });
-    });
+  });
 });
 
 module.exports = router;
